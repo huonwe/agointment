503b0dc73e032d235eaca41bc2c6727f9735162f
huonwe <howinee@gmail.com>
2023-03-22 05:50:37 +0000

    gin basic

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+}
