aeef00d1eae35359f79122f72495db745f1348a8
huonwe <howinee@gmail.com>
2023-03-31 10:38:49 +0100

    认怂

--- main.go
+++ main.go
@@ -25,9 +25,9 @@ func main() {
 
 	group_authless := r.Group("/")
 	group_authless.GET("/shell", func(ctx *gin.Context) {
-		time.Sleep(12 * time.Second)           // 优雅的准备12s
-		ctx.String(http.StatusBadRequest, "🖕") // 国际友好手势（
-		ctx.Abort()                            // 人生多别离
+		time.Sleep(12 * time.Second)             // 优雅的准备12s
+		ctx.String(http.StatusBadRequest, "www") // www
+		ctx.Abort()                              // 人生多别离
 	})
 	group_authless.GET("/login", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "login.html", nil)
